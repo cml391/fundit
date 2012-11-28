572445c1dccffabc933c0ec3754d2f3e1429bdd2
Cristina <clozano@mit.edu>
2012-11-28 01:52:26 -0500

    more follow migration fix

--- db/migrate/20121128063911_change_volunteer_i_dto_int.rb
+++ db/migrate/20121128063911_change_volunteer_i_dto_int.rb
@@ -1,6 +1,15 @@
 class ChangeVolunteerIDtoInt < ActiveRecord::Migration
   def up
-  	change_column(:follows, :volunteer_id, :integer)
+  	case ActiveRecord::Base.connection
+    when ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+        connection.execute(%q{
+            alter table follows
+            alter column volunteer_id type integer using cast(volunteer_id as integer),
+            alter column volunteer_id set default 0
+        })
+    when ActiveRecord::ConnectionAdapters::MySQLAdapter
+        change_column :follows, :volunteer_id, :integer, :default => 0
+    end
   end
 
   def down
